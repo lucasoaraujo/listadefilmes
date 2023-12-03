19e195f7b3a5a242ddf0bfd7c2d0d95824a2cc31
Lucas Araujo <lucasoliveira_0@icloud.com>
2023-12-03 11:03:39 -0300

    Projeto criado

---
+++ src/main/java/com/devlucasboaraujo/listaDeSeries/ListaDeSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.devlucasboaraujo.listaDeSeries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeSeriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlucasboaraujo/listaDeSeries/ListaDeSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucasboaraujo.listaDeSeries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
